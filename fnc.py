538175432475cfa6addbad0245631f9c49f80693
benvito <ganyasher@gmail.com>
2023-03-18 21:52:25 +0700

    percentages

--- fnc.py
+++ fnc.py
@@ -3,26 +3,53 @@ import pandas as pd
 
 
 class Build:
-    def __init__(self, motherboard=None, cpu=None, gpu=None, psu=None, ram=None, sum_price=None):
-        self.MB_per = 10
-        self.CPU_per = 30
-        self.GPU_per = 35
-        self.PSU_per = 10
-        self.RAM_per = 15
+    def __init__(self, motherboard=None, cpu=None, gpu=None, psu=None, ram=None, sum_price=None, rom=None, cfg=None):   
         self.motherboard_price = None
         self.cpu_price = None
         self.gpu_price = None
+        self.rom_price = None
+        self.ram_price = None
+        self.psu_price = None
+
+        if cfg == "Gaming":
+            self.MB_per = 13
+            self.CPU_per = 13
+            self.GPU_per = 35
+            self.PSU_per = 11
+            self.RAM_per = 11
+            self.ROM_per = 9
+            self.Other_per = 8
+        elif cfg == "Working":
+            self.MB_per = 13
+            self.CPU_per = 26
+            self.GPU_per = 22
+            self.PSU_per = 9
+            self.RAM_per = 13
+            self.ROM_per = 10
+            self.Other_per = 7
+        elif cfg == "Graphics":
+            self.MB_per = 11
+            self.CPU_per = 13
+            self.GPU_per = 37
+            self.PSU_per = 10
+            self.RAM_per = 12
+            self.ROM_per = 10
+            self.Other_per = 7
 
         self.motherboard = motherboard
         self.cpu = cpu
         self.gpu = gpu
         self.psu = psu
         self.ram = ram
+        self.rom = None
         self.sum_price = sum_price
     def set_price(self):
         self.motherboard_price = (int((self.sum_price / 100) * (self.MB_per - 2)), int((self.sum_price / 100) * self.MB_per))
         self.cpu_price = (int((self.sum_price / 100) * (self.CPU_per - 5)), int((self.sum_price / 100) * self.CPU_per))
         self.gpu_price = (int((self.sum_price / 100) * (self.GPU_per - 5)), int((self.sum_price / 100) * self.GPU_per))
+        self.rom_price = (int((self.sum_price / 100) * (self.ROM_per - 5)), int((self.sum_price / 100) * self.ROM_per))
+        self.ram_price = (int((self.sum_price / 100) * (self.RAM_per - 5)), int((self.sum_price / 100) * self.RAM_per))
+        self.psu_price = (int((self.sum_price / 100) * (self.PSU_per - 5)), int((self.sum_price / 100) * self.PSU_per))
     
 
 
