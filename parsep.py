f3ce263fa923783f52126800417ac8003ed3ebf9
benvito <ganyasher@gmail.com>
2023-03-29 10:01:43 +0700

    hz

--- parsep.py
+++ parsep.py
@@ -28,15 +28,70 @@ dns_filters = {
     "1qmwme" : "FS1",
 }
 
-page = 1
-socket = "lga1170"
+# page = 1
+# socket = "lga1170"
 
-url = f'https://zeon18.ru/computer-parts/power-supplies/'
+# url = f'https://www.newegg.com/p/pl?d=motherboard'
 
-r = requests.get(url)
-soup = bs(r.content, 'lxml')
-items = soup.find_all('a', class_="catalog-item-title")
+# r = requests.get(url)
+# soup = bs(r.text, 'lxml')
+# # items = soup.find_all('a', text = "Материнская плата MSI H510M-A PRO")
 
-print(items)
-for item in items:
-    print(item.text)
+# print(soup)
+# # for item in items:
+# #     print(item.text)
+
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import NoSuchElementException
+import time
+from typing import List, Tuple, Union
+
+
+def parse(url: str) -> List[Tuple[str, str, str]]:
+    options = Options()
+    options.add_argument('--headless')
+
+    items = []
+    driver = webdriver.Firefox(options=options)
+    driver.implicitly_wait(0.5)
+    
+    while True:
+        try:
+            page = last_page = 1
+            while page <= last_page:
+                if page > 0:
+                 url = f'https://www.citilink.ru/catalog/shiny/?p={page}'
+            
+            
+            for item_el in driver.find_elements_by_css_selector("product_data__gtm-js product_data__pageevents-js  ProductCardVertical js--ProductCardInListing ProductCardVertical_normal ProductCardVertical_shadow-hover ProductCardVertical_separated"):
+                
+
+                try:
+                    name = item_el.find_element_by_class_name('ProductCardVerticalLayout__wrapper-description ProductCardVertical__layout-description').text
+                except NoSuchElementException:
+                    name = ''
+
+                try:
+                    price = item_el.find_element_by_class_name('ProductPrice ProductPrice_default ProductCardVerticalPrice__price-current').text
+                except NoSuchElementException:
+                    price = '-'
+                    
+                row = name, price
+                items.append(row)
+            try:
+               last_page = int(item_el.find_element_by_class_name('PaginationWidget__page js--PaginationWidget__page PaginationWidget__page_last PaginationWidget__page-link').text)
+               page += 1   
+
+            except NoSuchElementException:
+                break
+            
+            
+        finally:
+            driver.quit()
+
+    return items
+
+if __name__ == '__main__':
+    url = 'https://www.citilink.ru/catalog/shiny/?p=1'
+    parse(url)
